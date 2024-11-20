c3a8495fcfdeb5efc64d28beef83b5b8587d7de3
Nestor Júnior <contato.nestor@gmail.com>
2024-11-20 11:28:28 -0300

    init project listme backend

---
+++ server.js
@@ -0,0 +1,14 @@
+// Pass Supabase: TojRH6pSi1SYDab2
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/users");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => console.log(`Server load port ${PORT}`));
